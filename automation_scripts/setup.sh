6069be3b008beff07444215ebf9ac64a90bed757
Azasange Tshaka <0312181158089@tpc.local>
2025-07-28 13:57:01 +0200

    final commit

--- automation_scripts/setup.sh
+++ automation_scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir project_folder/src/docs/
+mkdir -p project_folder/src project_folder/docs
 echo "console.log('Project started');" > project_folder/src/index.js
 echo "✅ Folder structure created successfully!"
 
